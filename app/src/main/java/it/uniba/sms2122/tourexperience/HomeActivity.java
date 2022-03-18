b756351d60259c1fac5cef14ba5dd81e136d0178
CatignanoFrancesco <catignanof@gmail.com>
2022-03-18 21:28:01 +0100

    Aggiunta la bottom navbar

--- app/src/main/java/it/uniba/sms2122/tourexperience/HomeActivity.java
+++ app/src/main/java/it/uniba/sms2122/tourexperience/HomeActivity.java
@@ -2,16 +2,20 @@ package it.uniba.sms2122.tourexperience;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        String title = getString(R.string.hello, "Francesco");  //TODO inserire nome vero
+        Objects.requireNonNull(getSupportActionBar()).setTitle(title);
+
         setContentView(R.layout.activity_home);
     }
 

6d27d98742550851fd842f1300790de526fc1a3f
CatignanoFrancesco <catignanof@gmail.com>
2022-03-18 19:33:45 +0100

    Aggiunta action bar alla schermata home

--- app/src/main/java/it/uniba/sms2122/tourexperience/HomeActivity.java
+++ app/src/main/java/it/uniba/sms2122/tourexperience/HomeActivity.java
@@ -2,7 +2,10 @@ package it.uniba.sms2122.tourexperience;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }

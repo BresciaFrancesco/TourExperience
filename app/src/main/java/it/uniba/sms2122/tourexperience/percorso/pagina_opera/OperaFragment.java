2b3b0922d10c058f862a2ee010dda861e66bfa37
GiannettaGerardo <ggp00519@protonmail.com>
2022-05-07 18:20:52 +0200

    Creare il fragment dell'opera nel percorso #25
    Impostato il fragment e sistemato il fragment ImageAndDescriptionFragment e l'adapter ImageAdapter

--- app/src/main/java/it/uniba/sms2122/tourexperience/percorso/pagina_opera/OperaFragment.java
+++ app/src/main/java/it/uniba/sms2122/tourexperience/percorso/pagina_opera/OperaFragment.java
@@ -5,20 +5,38 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import it.uniba.sms2122.tourexperience.R;
+import it.uniba.sms2122.tourexperience.imageanddescription.ImageAndDescriptionFragment;
+import it.uniba.sms2122.tourexperience.main.HomeFragment;
+import it.uniba.sms2122.tourexperience.model.Opera;
+import it.uniba.sms2122.tourexperience.utility.filesystem.LocalFilePercorsoManager;
 
 
 public class OperaFragment extends Fragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private final Opera opera;
+    private final String nomeStanza;
+    private final FragmentManager fragmentManager = getParentFragmentManager();
+    private ImageAndDescriptionFragment fragment;
+    private LocalFilePercorsoManager localFilePercorsoManager;
+    private final String IMG_EXTENSION = ".webp";
 
+    public OperaFragment(final Opera opera, final String nomeStanza) {
+        this.opera = opera;
+        this.nomeStanza = nomeStanza;
     }
 
     @Override
@@ -31,6 +49,23 @@ public class OperaFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        localFilePercorsoManager = new LocalFilePercorsoManager(getContext().getFilesDir().toString());
+        final List<String> immaginiOpera = Collections.singletonList(
+            Paths.get(
+                    localFilePercorsoManager.getGeneralPath(),
+                    "Stanze",
+                    nomeStanza,
+                    opera.getNome(),
+                    opera.getNome()+IMG_EXTENSION
+            ).toString()
+        );
+        fragment = new ImageAndDescriptionFragment(immaginiOpera, opera.getDescrizione());
+        fragmentManager.beginTransaction()
+            .setReorderingAllowed(true)
+            //.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right, R.anim.slide_in_right, R.anim.slide_out_left)
+            .add(R.id.imageanddescription_fragment_container_view, fragment)
+            .commit();
 
     }
 }

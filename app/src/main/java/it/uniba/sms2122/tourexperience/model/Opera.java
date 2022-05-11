9e7dd194c8f310980db2d67db2379ec2916acfce
GiannettaGerardo <ggp00519@protonmail.com>
2022-05-11 22:10:13 +0200

    Creare il fragment dell'opera nel percorso #25
    Aggiunti i metodi per il salvataggio e ripristino dello stato

--- app/src/main/java/it/uniba/sms2122/tourexperience/model/Opera.java
+++ app/src/main/java/it/uniba/sms2122/tourexperience/model/Opera.java
@@ -23,24 +23,12 @@ public class Opera {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getDescrizione() {
         return descrizione;
-    }
-
-    public void setDescrizione(String descrizione) {
-        this.descrizione = descrizione;
     }
 
     public String getPercorsoImg() {
--- app/src/main/java/it/uniba/sms2122/tourexperience/percorso/pagina_opera/OperaFragment.java
+++ app/src/main/java/it/uniba/sms2122/tourexperience/percorso/pagina_opera/OperaFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainerView;
 import androidx.fragment.app.FragmentManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +23,8 @@ import java.util.List;
 import it.uniba.sms2122.tourexperience.R;
 import it.uniba.sms2122.tourexperience.imageanddescription.ImageAndDescriptionFragment;
 import it.uniba.sms2122.tourexperience.main.HomeFragment;
+import it.uniba.sms2122.tourexperience.main.MainActivity;
+import it.uniba.sms2122.tourexperience.model.Museo;
 import it.uniba.sms2122.tourexperience.model.Opera;
 import it.uniba.sms2122.tourexperience.utility.filesystem.LocalFilePercorsoManager;
 
@@ -54,21 +58,40 @@ public class OperaFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         localFilePercorsoManager = new LocalFilePercorsoManager(getContext().getFilesDir().toString());
-        final List<String> immaginiOpera = Collections.singletonList(
-            Paths.get(
-                    localFilePercorsoManager.getGeneralPath(),
-                    "Stanze",
-                    nomeStanza,
-                    opera.getNome(),
-                    opera.getNome()+IMG_EXTENSION
-            ).toString()
-        );
+        final List<String> immaginiOpera = Collections.singletonList(opera.getPercorsoImg());
         fragment = new ImageAndDescriptionFragment(immaginiOpera, opera.getDescrizione());
         fragmentManager.beginTransaction()
             .setReorderingAllowed(true)
             //.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right, R.anim.slide_in_right, R.anim.slide_out_left)
             .add(R.id.imageanddescription_fragment_container_view, fragment)
             .commit();
+    }
 
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.v("OperaFragment", "chiamato onSaveInstanceState()");
+        if (opera != null) {
+            outState.putString("id", opera.getId());
+            outState.putString("nome", opera.getNome());
+            outState.putString("percorsoImg", opera.getPercorsoImg());
+            outState.putString("descrizione", opera.getDescrizione());
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    public void onViewStateRestored(@NonNull Bundle savedInstanceState) {
+        Log.v("OperaFragment", "chiamato onViewStateRestored()");
+        if(savedInstanceState != null && !savedInstanceState.isEmpty()) {
+            this.opera = new Opera(
+                savedInstanceState.getString("id"),
+                savedInstanceState.getString("nome"),
+                savedInstanceState.getString("percorsoImg"),
+                savedInstanceState.getString("descrizione")
+            );
+        }
+        super.onViewStateRestored(savedInstanceState);
     }
 }
